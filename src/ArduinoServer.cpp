54c473cc377cde0798847e44616e1461e62f3935
Marisa Chang <mtchang@andrew.cmu.edu>
2015-04-14 03:50:49 -0400

    added (probably non-working) server code

---
+++ src/ArduinoServer.cpp
@@ -0,0 +1,93 @@
+//
+// A simple Internet server application.
+// It listens to the port written in command line (default 1234),
+// accepts a connection, and sends the "Hello!\r\n" message
+// to a client. Then it receives the answer from a client and terminates.
+//
+// Usage:
+//      server [port_to_listen]
+// Default is the port 1234.
+//
+// taken from: http://mech.math.msu.su/~vvb/Java/samples/Invert/server.cpp
+//
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+int main(int argc, char *argv[]) {    
+    int listenPort = 1234;
+    if (argc > 1)
+        listenPort = atoi(argv[1]);
+
+    // Create a socket
+    int s0 = socket(AF_INET, SOCK_STREAM, 0);
+    if (s0 < 0) {
+        perror("Cannot create a socket"); exit(1);
+    }
+
+    // Fill in the address structure containing self address
+    struct sockaddr_in myaddr;
+    memset(&myaddr, 0, sizeof(struct sockaddr_in));
+    myaddr.sin_family = AF_INET;
+    myaddr.sin_port = htons(listenPort);        // Port to listen
+    myaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    // Bind a socket to the address
+    int res = bind(s0, (struct sockaddr*) &myaddr, sizeof(myaddr));
+    if (res < 0) {
+        perror("Cannot bind a socket"); exit(1);
+    }
+
+    // Set the "LINGER" timeout to zero, to close the listen socket
+    // immediately at program termination.
+    struct linger linger_opt = { 1, 0 }; // Linger active, timeout 0
+    setsockopt(s0, SOL_SOCKET, SO_LINGER, &linger_opt, sizeof(linger_opt));
+
+    // Now, listen for a connection
+    // TODO: "1" is the maximal length of the queue
+    while (listen(s0, 1) < 0) {
+        // Accept a connection
+        struct sockaddr_in peeraddr;
+        socklen_t peeraddr_len;
+        int s1 = accept(s0, (struct sockaddr*) &peeraddr, &peeraddr_len);
+        if (s1 < 0) {
+            perror("Cannot accept"); 
+            continue;
+        }
+
+        char buffer[1024];
+        read(s1, buffer, 1023); 
+        /* TODO: find out format of data sent from local laptop (Arduino)
+         * NEED: which sensor was triggered */
+        int sensor = 0; //TODO: figure out labeling for sensors 
+        
+        // send data over to main server 
+        // Create a socket 
+        // TODO: do i need to change this?
+        int client_socket = socket(AF_INET, SOCK_STREAM, 0);
+        if (s0 < 0) {
+            perror("Cannot create a socket"); 
+            exit(1);
+                           
+        }
+        
+        //TODO: Fill in the address of bridge server
+        sockaddr_in bridgeaddr;
+        
+        //Connect to bridge server
+        connect(client_socket, (struct sockaddr*) &bridgeaddr, sizeof(bridgeaddr));
+        char s [1] = {(char) sensor};
+
+        write(client_socket, s, sizeof(int));
+        close(client_socket);  
+        close(s1);          // Close the data socket
+    }
+
+    res = close(s0);    // Close the listen socket
+    return 0;
+}
