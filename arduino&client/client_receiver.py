f679878cb30d76eea1471681919dafe0040a9c53
Marisa Chang <mtchang@andrew.cmu.edu>
2015-04-22 22:29:43 -0400

    .

--- arduino&client/client_receiver.py
+++ arduino&client/client_receiver.py
@@ -4,8 +4,8 @@ import socket
 import sys
 import time
 
-#host = 'pbridge.adm.cs.cmu.edu'
-host = '' #fills in address of host machine
+host = 'pbridge.adm.cs.cmu.edu'
+#host = '' #fills in address of host machine
 port = 15440
 
 #bailiang's computer
@@ -20,12 +20,16 @@ while 1:
     sock.connect(server_address)
     print >>sys.stderr, 'connecting to %s port %s' % server_address
    
-    message = ser.readline()
-  
+    message = ser.readline()  
     #message = raw_input("enter something:")
-    message = line
-    print >>sys.stderr, 'sending "%s"' % message
-    sock.sendall(message)
+
+    m_arr = message.split(' ')
+    sensor = m_arr[0]
+    velocity = m_arr[1]
+ 
+    print >>sys.stderr, 'sending: %s' % message
+    sock.send(sensor)
+    sock.send(velocity)
     time.sleep(1)
 
     print >>sys.stderr, 'closing socket'
