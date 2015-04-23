da1d649152cd77e47bce99bfdeb57a7de5785650
Marisa Chang <mtchang@andrew.cmu.edu>
2015-04-22 21:10:12 -0400

    fixed python client

--- arduino&client/client_receiver.py
+++ arduino&client/client_receiver.py
@@ -1,34 +1,33 @@
 import serial
-import pytz
+#import pytz
 import socket
 import sys
 import time
 
-host = 'pbridge.adm.cs.cmu.edu'
+#host = 'pbridge.adm.cs.cmu.edu'
+host = '' #fills in address of host machine
 port = 15440
-# ser = serial.Serial('/dev/cu.usbserial-A403I0LM', 9600)
-count = 0
-while  (count < 1):
-	# count = count + 1
-	# line = ser.readline()
-	# line_split = line.split(' ')
+
+#bailiang's computer
+#ser = serial.Serial('/dev/cu.usbserial-A403I0LM', 9600)
+
+#marisa's computer
+ser = serial.Serial('/dev/ttyUSB0', 9600)
+
+while 1:
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_address = (host, port)
     sock.connect(server_address)
     print >>sys.stderr, 'connecting to %s port %s' % server_address
-    # count = count + 1
-    
-    message = "1 0"
+   
+    message = ser.readline()
+  
+    #message = raw_input("enter something:")
+    message = line
     print >>sys.stderr, 'sending "%s"' % message
     sock.sendall(message)
     time.sleep(1)
+
     print >>sys.stderr, 'closing socket'
     sock.close()
 
-# ser = serial.Serial('/dev/cu.usbserial-A403I0LM', 9600)
-
-# while True:
-
-#   line = ser.readline()
-#   line_split = line.split(' ')
-#   print line_split[0], line_split[1] 
